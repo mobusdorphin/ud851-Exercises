637cf4eb0b875e8bc38b036e010aecb34502deb6
Mobus Dorphin <celestialtuba@gmail.com>
2018-04-04 01:50:07 -0400

    Had a whale of a time getting PreferenceFragementCompat to work.  But it eventually happened.

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,18 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by mobus on 4/4/18.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
