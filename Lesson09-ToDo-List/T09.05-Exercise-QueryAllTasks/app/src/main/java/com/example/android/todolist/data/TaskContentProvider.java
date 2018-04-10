5d8b66f19f509698a26e0a1eef26b2a08c9fe48b
Mobus Dorphin <celestialtuba@gmail.com>
2018-04-10 00:40:22 -0400

    Query implemented

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,35 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // DONE (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        // DONE (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
 
-        // TODO (3) Query for the tasks directory and write a default case
+        Cursor retCursor;
 
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        switch (match) {
+            case TASKS:
+               retCursor = db.query(TaskContract.TaskEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        // DONE (3) Query for the tasks directory and write a default case
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // DONE (4) Set a notification URI on the Cursor and return that Cursor
+
+        return retCursor;
     }
 
 
